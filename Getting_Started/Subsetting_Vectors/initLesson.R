09afe251d74bea93f735b9dfe64c7b5f2f06805b
Simon Queenborough <simon.queenborough@yale.edu>
2018-09-05 09:34:29 -0400

    Create initLesson.R

---
+++ Getting_Started/Subsetting_Vectors/initLesson.R
@@ -0,0 +1,9 @@
+# Put initialization code in this file. The variables you create
+# here will show up in the user's workspace when he or she begins
+# the lesson.
+
+
+swirl_options(swirl_logging = TRUE)
+
+x <- sample(c(rnorm(20), rep(NA, 20)))
+x[4] <- -2.61517749
