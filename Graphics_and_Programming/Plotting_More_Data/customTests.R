90ffc18c0d72796a79868c80757d3c5b19b15ee4
Simon Queenborough <simon.queenborough@yale.edu>
2018-11-10 17:26:15 -0500

    Update customTests.R

--- Graphics_and_Programming/Plotting_More_Data/customTests.R
+++ Graphics_and_Programming/Plotting_More_Data/customTests.R
@@ -14,7 +14,7 @@ getLog <- function(){
 submit_log <- function(){
   
   # Please edit the link below
-  pre_fill_link <- "https://docs.google.com/forms/d/e/1FAIpQLSf6GCAgZW_JepFYvzJgHq5yGxAiZLnIEeT0jHMybqmf0BuewA/viewform?usp=pp_url&entry.66552501"
+  pre_fill_link <- "https://docs.google.com/forms/d/e/1FAIpQLSecL4XD8z2CEFRhpLUotEI5L-n46pYeHIRB5Wop2UMHHubGMg/viewform?usp=pp_url&entry.440578360"
   
   # Do not edit the code below
   if(!grepl("=$", pre_fill_link)){
