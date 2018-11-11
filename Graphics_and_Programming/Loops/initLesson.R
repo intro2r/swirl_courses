dd7c0ee36275815928550537a87e39a7e9bb9898
Simon Queenborough <simon.queenborough@yale.edu>
2018-11-10 20:55:36 -0500

    Update initLesson.R

--- Graphics_and_Programming/Loops/initLesson.R
+++ Graphics_and_Programming/Loops/initLesson.R
@@ -3,3 +3,15 @@
 swirl_options(swirl_logging = TRUE)
 
 
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+# Path to installed lesson
+lessonpath <- file.path(.get_course_path(), "Graphics_and_Programming",
+                        "Loops")
+
+
+trees <- read.table(file = paste(lessonpath, '/treespecies_cleandata.txt', sep = ''), header = TRUE, sep = '\t')
